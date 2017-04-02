22efe7241f9feae341e2f5d0b77de2ccd9fa6122
kettoleon <kettoleon@gmail.com>
2017-04-02 15:48:32 +0200

    Added ability to build a succumber-reports-ui .jar from maven and used it to generate the reports from the cucumber Formatter.

---
+++ reports/succumber-reports-gherkin-acceptance-tests/src/test/resources/cucumber/SecondSimpleFeature.feature
@@ -0,0 +1,4 @@
+Feature: Another Simple feature
+
+  Scenario: Another Simple scenario in another simple feature
+    Given I have 8 cukes in my belly
--- reports/succumber-reports-gherkin/src/main/java/com/github/kettoleon/succumber/reports/gherkin/SuccumberFormatter.java
+++ reports/succumber-reports-gherkin/src/main/java/com/github/kettoleon/succumber/reports/gherkin/SuccumberFormatter.java
@@ -1,19 +1,29 @@
 package com.github.kettoleon.succumber.reports.gherkin;
 
+import com.google.gson.Gson;
 import gherkin.formatter.Formatter;
 import gherkin.formatter.Reporter;
 import gherkin.formatter.model.*;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class SuccumberFormatter implements Formatter, Reporter {
 
     private static final Logger log = LoggerFactory.getLogger(SuccumberFormatter.class);
 
-    public SuccumberFormatter(){
+    private List<Feature> features = new ArrayList<>();
+
+    public SuccumberFormatter() {
         log.trace("{} instantiated", SuccumberFormatter.class.getSimpleName());
         //Load DI context and configuration
     }
@@ -27,7 +37,7 @@ public class SuccumberFormatter implements Formatter, Reporter {
     }
 
     public void feature(Feature feature) {
-
+        features.add(feature);
     }
 
     public void scenarioOutline(ScenarioOutline scenarioOutline) {
@@ -63,9 +73,43 @@ public class SuccumberFormatter implements Formatter, Reporter {
     }
 
     public void close() {
+        try {
+        String targetFolder = "target/reports/succumber";
+        File featuresAssetFile = new File(targetFolder+"/assets/features.jsonp");
+        featuresAssetFile.getParentFile().mkdirs();
+        Gson gson = new Gson();
+        FileWriter fileWriter = new FileWriter(featuresAssetFile);
+        fileWriter.append("declareAsset({\n" +
+                "    \"asset\": \"features\",\n" +
+                "    \"data\":");
+        gson.toJson(features, fileWriter);
+        fileWriter.append("});");
+        fileWriter.close();
 
         //Close streams
-        //Copy reports-ui file
+        //Extract succumber-reports-ui folder
+        String resourcesFolder = "succumber-reports-ui";
+        InputStream folder = Thread.currentThread().getContextClassLoader().getResourceAsStream(resourcesFolder);
+
+
+            List<String> files = Arrays.asList(IOUtils.toString(folder, "UTF-8").split("\\n"));
+            for (String file : files) {
+                if (file.matches(".*\\..*")) {
+                    System.out.println("Unpacking: " + file);
+                    unpack(resourcesFolder + "/" + file, targetFolder + "/" + file);
+                } else {
+                    System.out.println("Skipping:  " + file);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private void unpack(String resource, String targetFile) throws IOException {
+        FileUtils.copyInputStreamToFile(Thread.currentThread().getContextClassLoader().getResourceAsStream(resource), new File(targetFile));
     }
 
     public void eof() {
